d71af1a700433c6e99759252b42c11d27d0813ae
Elise Reber <elise.reber@ens.uvsq.fr>
2021-04-16 10:57:33 +0200

    canvas et bouton démarrer

---
+++ projet avion.py
@@ -0,0 +1,57 @@
+#########################################
+# groupe DLBI 1
+# Kubilay MEYDAN
+# Julie CIESLA
+# Alix FRAGNER
+# Margaux DUBOIS GUENRO
+# Elise REBER
+# https://github.com/uvsq22004090/Projet-Avion.git
+#########################################
+
+
+import tkinter as tk
+
+racine = tk.Tk()
+racine.title("simulation d'avion")
+
+
+### CONSTANTES
+
+CANVAS_HEIGHT = 600
+CANVAS_WIDTH= 140
+COTE = 20
+
+### FONCTIONS
+
+def demarrer():
+    pass
+
+def quadrillage():
+    i = 0
+    j = 0
+    while i <= CANVAS_WIDTH:
+        while j <= CANVAS_HEIGHT:
+            avion.create_rectangle(i, j, i+COTE, j+COTE)
+            j += COTE
+        i += COTE
+        j = 0
+        print(i)
+            
+
+### WIDGETS
+
+avion = tk.Canvas(racine, height=CANVAS_HEIGHT, width=CANVAS_WIDTH, bg='blue')
+bouton_demarrer = tk.Button(racine, text='démarrer', command=demarrer)
+#Statistiques = 
+#informations_avion
+
+
+
+### POSITIONNEMENT
+
+avion.grid(row=0, column=1)
+bouton_demarrer.grid(row=0, column=0)
+
+avion.bind(quadrillage())
+
+racine.mainloop()
