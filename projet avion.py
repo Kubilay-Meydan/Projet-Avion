ea1a0d5dadd3212e3feb9eb5dfebcf8ea3885a59
Alix FRAGNER <alix.fragner@ens.uvsq.fr>
2021-05-02 21:18:41 +0200

    Ajout liste passagers

--- projet avion.py
+++ projet avion.py
@@ -57,6 +57,7 @@ def passagers(mat):
     mat.append([[x, y]])
 
     while mat[-1] in mat_2:
+        del mat[-1]
         x = choice([i for i in range(1, 8) if i not in interdit_x])
         y = choice([i for i in range(1, 31) if i not in interdit_y])
         mat.append([x, y])
